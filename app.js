259194e6d9b39ba27eb9e742fd7a1b1d83c41c5a
clairefromcork <clairefromcork@gmail.com>
2016-01-19 15:39:10 +0000

    Exploring object in JS

---
+++ app.js
@@ -0,0 +1,43 @@
+console.log ("Hello World");
+
+function add(a,b){
+    return a+b;
+    var ans = a + b;
+    return ans;
+}
+
+var theAns;
+theAns = add("5 + 10 is " + theAns);
+console.log (theAns);
+
+var name = "Claire"; //String
+var age = 21; //int
+var male = true; //boolean
+var myArray = newArray(); //Array
+
+if(age==21){
+    console.log ("Your're still young" + name);
+}
+
+var 0 = new Object();
+var anotherObject={};
+
+/*
+public class Person{
+public var name:String;
+public var age:int;
+public var male:boolean;
+}
+*/
+
+function Person(){
+    //default to female
+    //this.male = false;
+}
+
+//Somewhere else
+var p:Person = new Person();
+    p.name = "John";
+    p.age = 21;
+    p.male = true;
+  
